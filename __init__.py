07e4e17e2120d007490fb09af6583c2b7fdc499a
Curious-Junior <shlokbrar@gmail.com>
2023-03-31 23:13:31 -0700

    Added comments to and temprarily finalized __init__.py, main.py works except a little buggy backspace interaction

--- __init__.py
+++ __init__.py
@@ -1,18 +1,31 @@
-from sys import platform
+import sys, platform, subprocess, importlib, os # Packages either only required in __init__ or interfaced through __init__
+import re # regex
 
+__all__ = ["re"] # Initialize __all__ with regex
 
-import platform, subprocess, importlib
-import re, os
-
-__all__ = ["re", "os"]
-
+# Ensuring that the program works in both linux-based OS's and Windows(haven't considered MacOS)
 match platform.system():
-    case "Linux":
+    case "Linux": # If the OS is Linux
+        # Install the "getch" package required for live input
         subprocess.check_call([sys.executable, "-m", "pip", "install", "getch"],
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.STDOUT)
-        getch = importlib.import_module("getch").getch
-        __all__.append("getch")
+        getch = importlib.import_module("getch").getch # Import getch function so that it can be used directly
+        decode = lambda i: i # Dummy function to keep main.py (dummy because getch doesn't return bytes)
+        clear_screen = lambda: os.system("clear") # Different commands for windows and linux
+        to_byteStr = lambda i: bytes(i, "utf-8") # To byte string so that enter key and backspace key can be detectes
+        
+        backspace_code = b"\x7f" # Normal string to byte string version of backspace
+        enter_code = b"\n" # Just newline in byte form
+
+        __all__.extend(["getch", "clear_screen", "decode", "to_byteStr", "backspace_code", "enter_code"]) # Add all variables to __all__
     case "Windows":
-        getch = importlib.import_module("msvcrt").getch
-        __all__.append("getch")
+        getch = importlib.import_module("msvcrt").getch # Import msvcrt.getch (getch for windows) directly as it doesn't need to be installed
+        decode = lambda i: i.decode("utf-8") # Actual decode function(contaray to linux decode) as msvcrt getch returns byte strings
+        clear_screen = lambda: os.system("cls") # Clearn screen command for windows
+        to_byteStr = lambda i: i # Dummy to byte string function as msvcrt.getch already returns bytes
+
+        backspace_code = b"\r" # Return carriage(backspace) in bytes
+        enter_code = b"\x08" # Byte code for enter
+
+        __all__.extend(["getch", "clear_screen", "decode", "to_byteStr", "backspace_code", "enter_code"]) # Add all variables to __all__
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from __init__ import *
 
 def check_strength(i: str):
   patternTitles = ["Lowercase Letters: ", "Uppercase Letters: ","Numbers: ","Special characters: ","More than 8 characters: "]
-  patterns = ["([a-z]*)", "([A-Z])","([0-9]*)",r'''([!\@#\$%\^&\*\(\)~`\-\_=\+\[\{\]\}\|;:'",>/\?])''', "(.{8,})"]
+  patterns = ["([a-z])", "([A-Z])","([0-9])",r'''([!\@#\$%\^&\*\(\)~`\-\_=\+\[\{\]\}\|;:'",>/\?])''', "(.{8,})"]
   matches = []
   
   for p in patterns:
@@ -10,19 +10,22 @@ def check_strength(i: str):
     if(group and group != ""): matches.append("✔️"); continue
     matches.append("❌")
 
-  os.system("cls")
+  clear_screen()
   print(f'''Lowercase Letters: {matches[0]}
 Uppercase Letters: {matches[1]}
 Numbers: {matches[2]}
 Special characters: {matches[3]}
 More than 8 characters: {matches[4]}
-{i}''', flush=True)
+{i}
+{i[-1]}''', flush=True)
 
 liveStr = ""
 while True: 
     char = getch()
-    if(char == b"\r"): break
-    if(char == b"\x08"): liveStr = liveStr[:-1]
+    if(to_byteStr(char) == enter_code): break
+    if(to_byteStr(char) == backspace_code):
+        liveStr = liveStr[:-1]
 
-    liveStr += char.decode("utf-8")
+    liveStr += decode(char)
     check_strength(liveStr)
+    print(bytes(char, "utf-8"))
